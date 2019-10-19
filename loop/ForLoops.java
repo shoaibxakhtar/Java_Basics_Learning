2d7a58dc7028f7bf8b98736aa6ab2b4a364921bf
Shoaib Akhtar <44285309+shoaibxakhtar@users.noreply.github.com>
2019-10-19 11:03:55 +0530

    learning loops

---
+++ loop/ForLoops.java
@@ -0,0 +1,15 @@
+package loop;
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i = 2;
+		for (i  = 0; i<100; i= i+1) {
+			System.out.println("helloworld "+i);
+		}
+		i++;
+	}
+
+}
---
+++ module-info.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+/**
+ * @author shoaib
+ *
+ */
+module javaLearning {
+	exports Variables;
+	exports helloworld;
+	exports javabook_1;
+	exports operators;
+	exports typeCasting;
+	exports userinput;
+}
